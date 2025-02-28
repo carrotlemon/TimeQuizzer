1f676e9805e612f6fb78b91b21c0a3a6ca467bef
Trevor Kim <trevorkim3141@gmail.com>
2025-02-28 16:19:10 -0500

    style works now

--- src/main/main.js
+++ src/main/main.js
@@ -7,11 +7,11 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, './preload.js')
     }
   })
 
-  win.loadFile('index.html')
+  win.loadFile(path.join(__dirname, '../renderer/index.html'))
 }
 
 app.whenReady().then(() => {
---
+++ src/renderer/renderer.js
@@ -0,0 +1,3 @@
+// dynamicly changing css example
+// document.body.style.backgroundColor = "#f4f4f4";
+// document.body.style.fontFamily = "Arial, sans-serif";
